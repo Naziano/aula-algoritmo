787d2ab235d062c951624d6aa0d163a58a13cfde
Naziano Felix <nazianorn@gmail.com>
2019-07-01 20:41:28 -0300

    aula algoritmo01

---
+++ js/exemplo1.js
@@ -0,0 +1,28 @@
+  
+        //solicita valores
+        let valor1 = prompt('Digite o valor 1');
+        let valor2 = prompt('Digite o valor 2');
+
+        //coverte valores em inteiro
+        valor1 = parseInt(valor1);
+        valor2 = parseInt(valor2);
+
+        //realiza operações
+        let soma = valor1 + valor2;
+        let subtracao = valor1 - valor2;
+        let multiplicacao = valor1 * valor2;
+        let divisao = valor1 / valor2;
+        let potencia = valor1 ** valor2;
+        let resto = valor1 % valor2;
+
+        console.log('o resultado é' + divisao);
+
+
+    
+        //todo alert espera sempre que tenha um texto dentro dela.
+        // cria uma caixa de dialogo para confirmação.
+        //variavel independemte. (sempre let na frente da variavel).
+       
+        //Variavel= é um espaço criado pelo programador para armazenar informação para uso posterior (temporário).
+        //toda linguagem para numeros quebrados se usa o (ponto)
+

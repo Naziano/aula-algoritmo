469480173144ad1a707827c4d0a0398239efd628
Naziano Felix <nazianorn@gmail.com>
2019-07-01 21:28:00 -0300

    aula algoritimo novo

--- respostas-exercicio1/resposta2.js
+++ respostas-exercicio1/resposta2.js
@@ -1,1 +1,4 @@
-alert('O número informado foi');
+let numero =  parseInt(prompt('Digite um número:'));
+
+
+alert('O número informado foi'+ numero);
---
+++ respostas-exercicio1/resposta3.js
@@ -0,0 +1,5 @@
+let numero1 =  parseInt(prompt('Digite um número 1:'));
+let numero2 =  parseInt(prompt('Digite um número 2:'));
+
+let soma = numero1 + numero2;
+alert('A soma é: '+ soma);
